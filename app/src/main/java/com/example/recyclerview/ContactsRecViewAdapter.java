e9022f0716cdcb67d4b03db4f879489ef0b61fd6
Adarsh Goswami <adarsh010120@gmail.com>
2020-07-23 20:16:37 +0530

    RecyclerView1

--- app/src/main/java/com/example/recyclerview/ContactsRecViewAdapter.java
+++ app/src/main/java/com/example/recyclerview/ContactsRecViewAdapter.java
@@ -5,12 +5,16 @@ import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -24,12 +28,16 @@ public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecView
 
     public class ViewHolder extends RecyclerView.ViewHolder
     {
-        private RelativeLayout parent;
+        private CardView parent;
         private TextView txtName;
+        private TextView txtEmail;
+        private ImageView image;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtName= itemView.findViewById(R.id.txtName);
             parent= itemView.findViewById(R.id.parent);
+            txtEmail= itemView.findViewById(R.id.txtEmail);
+            image= itemView.findViewById(R.id.image);
         }
     }
 
@@ -44,12 +52,17 @@ public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecView
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         holder.txtName.setText(contacts.get(position).getName());
+        holder.txtEmail.setText(contacts.get(position).getEmail());
         holder.parent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(context, contacts.get(position).getName()+" Selected", Toast.LENGTH_SHORT).show();
             }
         });
+
+        Glide.with(context).asBitmap()
+                .load(contacts.get(position).getImgUrl())
+                .into(holder.image);
     }
 
     @Override
